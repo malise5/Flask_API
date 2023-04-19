9b2e0dd207e772ac4cd16f6075f42a80494be96c
Halkano Malise <kudezx@gmail.com>
2023-04-20 02:25:29 +0300

    migration

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, jsonify, make|_response, request
+from flask import Flask, jsonify, make_response, request
 from flask_migrate import Migrate
 
 from models import db, Production
 
-#initialize the App
+# initialize the App
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' #connects to the database
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #avoid an error
+# connects to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # avoid an error
+
+# migration
+migrate = Migrate(app, db)
+db.init_app(app)  # initialize the application with sqlalchemy
+
+# run the following in the terminal/shell
+# export FLASK_APP=app.py
+# export FLASK_RUN_PORT=5555
+# flask db init
+# flask db revision --autogenerate -m "create production table"
+# flask db upgrade
