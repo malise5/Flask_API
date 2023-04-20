a0fa376563dffe6ad63e453c034c9d484b48763c
Halkano Malise <kudezx@gmail.com>
2023-04-20 09:50:37 +0300

    new updates

--- app.py
+++ app.py
@@ -13,9 +13,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # avoid an error
 migrate = Migrate(app, db)
 db.init_app(app)  # initialize the application with sqlalchemy
 
+
+# Decorator
+@app.route('/')
+def index():
+    return '<h1>Hallo World!!</h1>'
+
+
+@app.route('/image')
+def image():
+    return '<h1>This is an image</h1>'
+
+
 # run the following in the terminal/shell
 # export FLASK_APP=app.py
 # export FLASK_RUN_PORT=5555
+
+# for creatind db migartion
 # flask db init
 # flask db revision --autogenerate -m "create production table"
 # flask db upgrade
+
+
+# flask run --debug
